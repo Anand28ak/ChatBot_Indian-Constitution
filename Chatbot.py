22f28c99f415da6e8d0e2ddec679d4228262ad0f
Anand28ak <anand130696@gmail.com>
2025-09-19 23:45:30 +0530

    Adding Pyhton Code

--- Chatbot.py
+++ Chatbot.py
@@ -1,1 +1,60 @@
 #This is chatbot code.
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain_community.chat_models import ChatOpenAI
+
+OPENAI_API_KEY = "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"
+#Upload PDF file
+st.header("Indian Constitution Chatbot")
+
+with st.sidebar:
+    st.title("Source Document")
+    file = st.file_uploader("Upload a PDF file and start asking questions", type="pdf")
+
+#Extract the file
+if file is not None:
+    pdf_reader = PdfReader(file)
+    text=""
+    for page in pdf_reader.pages:
+        text += page.extract_text()
+        #st.write(text)
+
+#Break it into chunks
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators="\n",
+        chunk_size=1000,
+        chunk_overlap=150,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    #st.write(chunks)
+
+    #generating Embeddings
+    embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+
+    #creating Vector Stores
+    vector_store = FAISS.from_texts(chunks,embeddings)
+
+    #get user question
+    user_question = st.text_input("Enter your question")
+
+    #do similarity search
+    if user_question:
+        match = vector_store.similarity_search(user_question)
+
+        llm = ChatOpenAI(
+            openai_api_key=OPENAI_API_KEY,
+            temperature = 0,
+            max_tokens = 2000,
+            model_name="gpt-3.5-turbo"
+        )
+        #output results
+        chain = load_qa_chain(llm, chain_type="stuff")
+        response = chain.run(input_documents = match, question=user_question)
+        st.write(response)
+
+#End of code.
